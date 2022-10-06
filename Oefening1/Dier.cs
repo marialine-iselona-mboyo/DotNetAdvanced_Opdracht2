d2944944acaaab3d015bafa74d1d458a409ad239
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-10-06 20:11:12 +0200

    Starting Oef1

---
+++ Oefening1/Dier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huisdieren
+{
+    internal class Dier
+    {
+        public double gewicht;
+
+        private string uitspraak;
+
+        public string zegt()
+        {
+            return uitspraak;
+        }
+    }
+}
